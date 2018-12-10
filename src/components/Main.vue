d6f488fbc9cf3bcc5532fb5e0c84ce4acf2653ae
minho0078 <minho0078@gmail.com>
2018-12-11 00:45:13 +0900

    first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+// import store from '@/store'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  computed: {
+    ...mapState({
+      msg: state => state.msg
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Main />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Main from '@/components/Main.vue'
+
+export default {
+  name: 'main',
+  components: {
+    Main
+  }
+}
+</script>
